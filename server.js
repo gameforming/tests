24a712ef02e6fa1f8c9c6f2c4e5ccbefa733736b
gameforming <96012344@student.theresialyceum.nl>
2026-05-20 13:59:47 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-// server.js
 const express = require("express");
 const http = require("http");
 const { Server } = require("socket.io");
+const fs = require("fs");
 
 const app = express();
 const server = http.createServer(app);
@@ -9,23 +9,52 @@ const io = new Server(server);
 
 app.use(express.static(__dirname));
 
-let messages = [];
+const FILE = "./messages.json";
+
+// load messages
+function loadMessages() {
+  if (!fs.existsSync(FILE)) return [];
+  return JSON.parse(fs.readFileSync(FILE, "utf8"));
+}
+
+// save messages
+function saveMessages(messages) {
+  fs.writeFileSync(FILE, JSON.stringify(messages, null, 2));
+}
+
+let messages = loadMessages();
 
 io.on("connection", (socket) => {
 
-  // stuur oude berichten
   socket.emit("load_messages", messages);
 
+  // send message
   socket.on("chat_message", (data) => {
 
-    messages.push(data);
+    const msg = {
+      id: Date.now().toString(),
+      name: data.name,
+      message: data.message
+    };
 
-    // infinite systeem
+    messages.push(msg);
+
+    // keep last 1500
     if (messages.length > 1500) {
-      messages.splice(0, 150);
+      messages = messages.slice(-1500);
     }
 
-    io.emit("chat_message", data);
+    saveMessages(messages);
+
+    io.emit("chat_message", msg);
+  });
+
+  // delete message
+  socket.on("delete_message", (id) => {
+    messages = messages.filter(m => m.id !== id);
+    saveMessages(messages);
+
+    io.emit("message_deleted", id);
   });
 
 });
